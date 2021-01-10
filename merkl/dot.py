f8ae67c675c5df2756ae2dd64d77fab88a644c43
Martin Pettersson <me@martindbp.com>
2021-01-10 15:45:24 +0100

    Print function deps only once

--- merkl/dot.py
+++ merkl/dot.py
@@ -11,24 +11,30 @@ def print_dot_graph_nodes(futures, target_fn=None, printed=set()):
     for future in futures:
         node_id = future.hash[:6]
         node_label = future.hash[:4]
-        fn_hash = future.code_args_hash[:6]
+        code_args_hash = future.code_args_hash[:6]
         fn_name = f'{future.fn.__name__}: {future.fn_code_hash[:4]}'
-        if fn_hash not in printed:
-            clamped = len(future.deps) > MAX_DEPS + 1
-            deps = future.deps
-            if clamped:
-                deps = deps[:MAX_DEPS]
+        if code_args_hash not in printed:
+            if future.fn_code_hash not in printed:
+                # Only print a function's deps once, in case of multiple invocations (list may be long)
+                clamped = len(future.deps) > MAX_DEPS + 1
+                deps = future.deps
+                if clamped:
+                    deps = deps[:MAX_DEPS]
 
-            deps = '|'.join(dep[:MAX_LEN] for dep in deps)
-            if clamped:
-                deps += f'|... +{len(future.deps)-MAX_DEPS}'
+                deps = '|'.join(dep[:MAX_LEN] for dep in deps)
+                if clamped:
+                    deps += f'|... +{len(future.deps)-MAX_DEPS}'
 
-            label = fn_name
-            if len(future.deps) > 0:
-                label = f'{{{label}|{deps} }}'
+                label = fn_name
+                if len(future.deps) > 0:
+                    label = f'{{{label}|{deps} }}'
 
-            print(f'\t"fn_{fn_hash}" [shape=record, label="{label}"];')
-            printed.add(fn_hash)
+                printed.add(future.fn_code_hash)
+            else:
+                label = fn_name
+
+            print(f'\t"fn_{code_args_hash}" [shape=record, label="{label}"];')
+            printed.add(code_args_hash)
             args_str = ''
             for key, val in future.bound_args.arguments.items():
                 if len(nested_collect(val, lambda x: isinstance(x, Future))) > 0:
@@ -36,19 +42,21 @@ def print_dot_graph_nodes(futures, target_fn=None, printed=set()):
 
                 args_str += (f'{key}={val}')[:MAX_LEN] + '\n'
 
-            print(f'\t"fn_{fn_hash}_args" [shape=box, label="{args_str}"];')
-            print(f'\t"fn_{fn_hash}_args" -> "fn_{fn_hash}";')
+            args_str = args_str.strip()
+
+            print(f'\t"fn_{code_args_hash}_args" [shape=box, label="{args_str}"];')
+            print(f'\t"fn_{code_args_hash}_args" -> "fn_{code_args_hash}";')
 
         if node_id not in printed:
             color = 'green' if future.in_cache() else 'red'
             label = f"< <font color='{color}'>{node_label}</font> >"
             print(f'\t"out_{node_id}" [shape=box, style=dotted, label={label}];')
-            print(f'\t"fn_{fn_hash}" -> "out_{node_id}"')
+            print(f'\t"fn_{code_args_hash}" -> "out_{node_id}"')
             printed.add(node_id)
             if target_fn:
                 print(f'\t"out_{node_id}" -> "fn_{target_fn}"')
 
-        print_dot_graph_nodes(future.parent_futures(), fn_hash, printed)
+        print_dot_graph_nodes(future.parent_futures(), code_args_hash, printed)
 
 
 def print_dot_graph(futures):
