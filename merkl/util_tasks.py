d76f8bb1017e56a741faf3e642141cc7c044f3bd
Martin Pettersson <me@martindbp.com>
2022-02-16 14:40:26 +0100

    Allow duplicates in 'combine_file_refs'

--- merkl/util_tasks.py
+++ merkl/util_tasks.py
@@ -6,6 +6,11 @@ from merkl.io import DirRef, FileRef
 def combine_file_refs(file_refs, link=True):
     dir_ref = DirRef()
     for file_ref in file_refs:
-        dir_ref.add_file_ref(file_ref, link=link)
+        print(file_ref)
+        try:
+            dir_ref.add_file_ref(file_ref, link=link)
+        except:
+            # Duplicate?
+            pass
 
     return dir_ref
