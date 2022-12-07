abd71102b45bacbd0129915430f54a9a558d678d
Martin Pettersson <me@martindbp.com>
2022-12-07 20:18:39 +0100

    Fix release problems

--- setup.py
+++ setup.py
@@ -9,14 +9,18 @@ except ImportError: # for pip <= 9.0.3
 
 long_description = open('README.md', 'r').read()
 
-install_reqs = parse_requirements('requirements.txt', session='hack')
-reqs = [str(ir.requirement) for ir in install_reqs]
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = list(install_reqs)
+try:
+    reqs = [str(ir.req) for ir in reqs]
+except:
+    reqs = [str(ir.requirement) for ir in reqs]
 
 setup(
     name="merkl",
     packages=find_packages(),
     entry_points={"console_scripts": ['merkl = merkl:main']},
-    version="0.3",
+    version="0.4",
     description="MerkL",
     long_description=long_description,
     long_description_content_type='text/markdown',
